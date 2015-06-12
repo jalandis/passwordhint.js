ffc99c0e8dc07f93e385038a2c7225ec5fb7e3df
John Landis <jalandis@gmail.com>
2015-06-11 21:34:03 -0400

    Changed from a module pattern to prototype pattern.

--- passwordhint.js
+++ passwordhint.js
@@ -1,127 +1,111 @@
-var Util = (function (Util) {
-    var intervalId = null;
+var PasswordHint = function(field, options) {
+  this.passwordField = field;
+  this.hiddenPassword = field.cloneNode(true);
+  this.intervalId = null;
 
-    var handleKeypress = function (keypress) {
+  this.obscureCharacter = options && options.hasOwnProperty('obscureCharacter') ? options.obscureCharacter : '\u25CF';
+  this.delay = options && options.hasOwnProperty('delay') ? options.delay : 1000;
+};
 
-        if (keypress.which == 0) {
-            return;
-        }
+PasswordHint.prototype.replaceCharacters = function (string, place, length, replacement) {
+  return string.slice(0, place) + replacement + string.slice(place + length);
+};
 
-        keypress.preventDefault();
-        if (intervalId) {
-            clearTimeout(intervalId);
-        }
+PasswordHint.prototype.deletePreviousCharacter = function (string, place) {
+  return (place == 0) ? string : this.replaceCharacters(string, place - 1, 1, '');
+};
 
-        var passwordField = keypress.target,
-            hiddenPassword = document.getElementById(passwordField.id + '_secret'),
-            caretPosition = passwordField.selectionStart,
-            selectionSize = passwordField.selectionEnd - passwordField.selectionStart,
-            character = String.fromCharCode(keypress.which);
+PasswordHint.prototype.deleteNextCharacter = function (string, place) {
+  return this.replaceCharacters(string, place, 1, '');
+};
 
-        hiddenPassword.value = replaceCharacters(hiddenPassword.value, caretPosition, selectionSize, character);
-        passwordField.value = replaceCharacters(generateStars(hiddenPassword), caretPosition, 1, character);
-        passwordField.setSelectionRange(caretPosition + 1, caretPosition + 1);
+PasswordHint.prototype.deleteSelection = function (string, place, length) {
+  return this.replaceCharacters(string, place, length, '');
+};
 
-        intervalId = setTimeout(function () {
-            var caretStart = passwordField.selectionStart,
-                caretEnd = passwordField.selectionEnd;
-            passwordField.value = generateStars(hiddenPassword);
-            passwordField.setSelectionRange(caretStart, caretEnd);
-            intervalId = null;
-        }, 1000);
-    };
+PasswordHint.prototype.generateStars = function (string) {
+  return Array(string.length + 1).join(this.obscureCharacter);
+};
 
-    var ignoreCharacter = function(charCode) {
-        return charCode == 0;
-    };
+PasswordHint.prototype.islibSupported = function () {
+  return typeof window.matchMedia === 'function' && typeof this.passwordField.setSelectionRange === 'function';
+};
 
-    var handleKeydown = function (keydown) {
+PasswordHint.prototype.isMobileDevice = function () {
+  return window.matchMedia('only screen and (max-width: 760px)').matches;
+};
 
-        if (keydown.which == 8 || keydown.which == 46) {
-            keydown.preventDefault();
-            if (intervalId) {
-                clearTimeout(intervalId);
-            }
-            var passwordField = keydown.target,
-                hiddenPassword = document.getElementById(passwordField.id + '_secret'),
-                selectionSize = passwordField.selectionEnd - passwordField.selectionStart,
-                caretPosition = passwordField.selectionStart;
+PasswordHint.prototype.hidePassword = function() {
+  var start = this.passwordField.selectionStart,
+      end = this.passwordField.selectionEnd;
 
-            // Delete highlighted or single character
-            if (selectionSize != 0) {
-                hiddenPassword.value = deleteSelection(hiddenPassword.value, caretPosition, selectionSize);
-            } else if (keydown.which == 46) {
-                hiddenPassword.value = deleteNextCharacter(hiddenPassword.value, caretPosition);
-            } else {
-                hiddenPassword.value = deletePreviousCharacter(hiddenPassword.value, caretPosition);
-                caretPosition--;
-            }
-            passwordField.value = generateStars(hiddenPassword);
-            passwordField.setSelectionRange(caretPosition, caretPosition);
-        }
-    };
+  this.passwordField.value = this.generateStars(this.hiddenPassword.value);
+  this.passwordField.setSelectionRange(start, end);
+  this.intervalId = null;
+};
 
-    var deletePreviousCharacter = function (string, place) {
+PasswordHint.prototype.ignoreCharacter = function(charCode) {
+  return charCode == 0;
+};
 
-        if (place == 0) {
-            return string;
-        } else {
-            return replaceCharacters(string, place - 1, 1, '');
-        }
-    };
+PasswordHint.prototype.handleKeypress = function (keypress) {
 
-    var deleteNextCharacter = function (string, place) {
-        return replaceCharacters(string, place, 1, '');
-    };
+  if (keypress.which == 0) {
+    return;
+  }
 
-    var deleteSelection = function (string, place, length) {
-        return replaceCharacters(string, place, length, '');
-    };
+  keypress.preventDefault();
+  if (this.intervalId) {
+    clearTimeout(this.intervalId);
+  }
 
-    var replaceCharacters = function (string, place, length, replacement) {
-        return string.slice(0, place) + replacement + string.slice(place + length);
-    };
+  var caretPosition = this.passwordField.selectionStart,
+      selectionSize = this.passwordField.selectionEnd - this.passwordField.selectionStart,
+      character = String.fromCharCode(keypress.which);
 
-    var generateStars = function (field) {
-        return Array(field.value.length + 1).join("*");
-    };
+  this.hiddenPassword.value = this.replaceCharacters(this.hiddenPassword.value, caretPosition, selectionSize, character);
+  this.passwordField.value = this.replaceCharacters(this.generateStars(this.hiddenPassword.value), caretPosition, 1, character);
+  this.passwordField.setSelectionRange(caretPosition + 1, caretPosition + 1);
 
-    var libSupported = function (field) {
-        if (typeof window.matchMedia !== "function") {
-            return false;
-        }
+  this.intervalId = setTimeout(this.hidePassword.bind(this), this.delay);
+};
 
-        if (typeof field.setSelectionRange !== 'function') {
-            return false;
-        }
+PasswordHint.prototype.handleKeydown = function (keydown) {
 
-        return true;
-    };
+  if (keydown.which == 8 || keydown.which == 46) {
+    keydown.preventDefault();
+    if (this.intervalId) {
+      clearTimeout(this.intervalId);
+    }
 
-    Util.createHint = function (field) {
+    var selectionSize = this.passwordField.selectionEnd - this.passwordField.selectionStart,
+        caretPosition = this.passwordField.selectionStart;
 
-        if (!libSupported(field)) {
-            return;
-        }
+    // Delete highlighted or single character
+    if (selectionSize != 0) {
+      this.hiddenPassword.value = this.deleteSelection(this.hiddenPassword.value, caretPosition, selectionSize);
+    } else if (keydown.which == 46) {
+      this.hiddenPassword.value = this.deleteNextCharacter(this.hiddenPassword.value, caretPosition);
+    } else {
+      this.hiddenPassword.value = this.deletePreviousCharacter(this.hiddenPassword.value, caretPosition);
+      caretPosition--;
+    }
+    this.passwordField.value = this.generateStars(this.hiddenPassword.value);
+    this.passwordField.setSelectionRange(caretPosition, caretPosition);
+  }
+};
 
-        var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+PasswordHint.prototype.startHinting = function () {
+  if (!this.islibSupported() || this.isMobileDevice()) {
+      return;
+  }
 
-        if (isMobile.matches) {
-            return;
-        }
+  this.hiddenPassword.id = this.passwordField.id + '_secret';
+  this.hiddenPassword.className += ' hide';
+  this.passwordField.type = 'text';
 
-        passwordField = field;
+  this.passwordField.parentNode.appendChild(this.hiddenPassword);
 
-        hiddenPassword = passwordField.cloneNode(true);
-        hiddenPassword.id = passwordField.id + '_secret';
-        hiddenPassword.className += ' hide';
-        passwordField.type = 'text';
-
-        passwordField.parentNode.appendChild(hiddenPassword);
-
-        passwordField.onkeypress = handleKeypress;
-        passwordField.onkeydown = handleKeydown;
-    };
-
-    return Util;
-})(Util || {});
+  this.passwordField.onkeypress = this.handleKeypress.bind(this);;
+  this.passwordField.onkeydown = this.handleKeydown.bind(this);;
+};
